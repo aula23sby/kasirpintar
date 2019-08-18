52722e112c7b377e72b29a5e3e4b6f7fd9c19e91
Aula <aula23sby@gmail.com>
2019-08-18 15:08:20 +0700

    tambah http

--- privasi/resources/views/homepage.blade.php
+++ privasi/resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
    <div class="jumbotron">
     <div class="text-center">
-        <h1>Kudou.com</h1>
+        <h1>http://Kudou.com</h1>
         <p>CRUD Applicaiotn Laravel and Vue</p>
     </div>
       <router-view></router-view>
